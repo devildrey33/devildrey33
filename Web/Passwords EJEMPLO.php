c84a7d8b6e3810b98b82feedb9feb20acd43270d
Josep Antoni Bover <devildrey33@hotmail.com>
2019-12-12 23:46:32 +0100

    Arxiu Passwords.php d'exemple...
    - Vaig perdre l'arxiu de passwords en l'ultim HardDiskWipe.. i ara l'he recuperat minimament i he fet un arxiu de mostra pel pròxim HDWipe

---
+++ Web/Passwords EJEMPLO.php
@@ -0,0 +1,24 @@
+<?php
+
+    return Array(
+        // BD Hostinger
+        "URL-BD-H"          => "",
+        "USER-BD-H"         => "",
+        "PASS-BD-H"         => "",
+        "NOM-BD-H"          => "",
+        // BD 1&1
+        "URL-BD"            => "",
+        "USER-BD"           => "",
+        "PASS-BD"           => "",
+        "NOM-BD"            => "",
+        // BD Local
+        "PASS-BD-LOCAL"     => "",
+        // Login y pass para admin
+        "LOGIN-devildrey33" => "admin",
+        "PASS-devildrey33"  => "admin",
+        // Login y pass para usuario avanzado (eliminar comentarios, y opciones básicas)
+        "LOGIN-Barba"       => "user",
+        "PASS-Barba"        => "user",
+    );
+
+?>
